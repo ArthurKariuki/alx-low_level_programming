e83c04f3c6cea393f51135a18bf30f64f89a966c
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-16 13:16:32 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ int main(void)
  * Return: 0 if success.
  */
 { 
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
