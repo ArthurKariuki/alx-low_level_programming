fd6700984b30f8ff82fbf971c67a4e2dd9f196b4
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-16 13:53:40 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * How to use printf for std output
- * the program is returning 0 if it is successful
+ * Description: main - How to use printf for std output
+ * Return: 0 if it is successful
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
