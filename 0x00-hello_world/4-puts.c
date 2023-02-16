921b058f7ffeb14402c5a58a963a7f0bad0ad4f0
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-16 03:56:25 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main(void)
 { 
-	puts ("with proper grammar, but the outcome is a piece of art,\n");
+	puts ("Programming is like building a multilingual puzzle\n");
 	return 0;
 
 }
