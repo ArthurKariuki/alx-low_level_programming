4a6d55131c700f22db7c73dc451cccf829e4b7d7
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-16 13:25:59 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,5 +4,7 @@ int main(void)
  * Description:main-Prints out a string to stdout.
  * Return:0ifsuccess.
  */
-{puts("\"Programming is like building a multilingual puzzle");
-return (0);}
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
