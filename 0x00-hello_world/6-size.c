1084056282b4e09808b29dc2a49086205bec84ca
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-16 15:00:57 -0800

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
- *Description: main - print the size of variables and space they consume in the memory
+ *Description: main - print the size of variables
  *Return: 0 if successful
  */
-int main()
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
