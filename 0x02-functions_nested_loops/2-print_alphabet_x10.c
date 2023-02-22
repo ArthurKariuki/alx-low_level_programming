301edc95ddb8fcf91a4fef15a74cbeaf6ecfbb12
arthurkariuki <arthurkariuki588@gmail.com>
2023-02-22 03:34:40 -0800

    alphabet*10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alphabet;
+
+	while (count++ <= 9)
+
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+		_putchar('\n');
+	}
+}
